07fd94cf66173fe7a1d190bb4993892e478d66c2
Al-Amen <83497300+Al-amen@users.noreply.github.com>
2025-01-05 23:52:51 +0600

    Time: 0 ms (100%), Space: 22.6 MB (35.12%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        
+        int i = 0;
+        for(int j = 0; j < nums.size(); j++) {
+            if(nums[j] != nums[i]) {
+                nums[i+1] = nums[j];
+                i++;
+            }
+        } 
+        return (i+1);
+    }
+};
