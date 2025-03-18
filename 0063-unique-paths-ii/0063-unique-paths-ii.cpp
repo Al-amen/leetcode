80dd5fcb4665f3c9d2be1f31e089756e62ad4737
Al-Amen <83497300+Al-amen@users.noreply.github.com>
2025-03-18 21:16:11 +0600

    Time: 0 ms (100%), Space: 11.9 MB (28.42%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -15,8 +15,25 @@ public:
     int uniquePathsWithObstacles(vector<vector<int>>& obstacleGrid) {
         int m = obstacleGrid.size();
         int n = obstacleGrid[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return total_uniquePathsWithObstacles(obstacleGrid,m-1,n-1,dp);
+        vector<vector<int>>dp(m,vector<int>(n,0));
+        dp[0][0] = 1;
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++) {
+                if(obstacleGrid[i][j] == 1)dp[i][j] = 0;
+                else {
+                     int up = (i > 0) ? dp[i-1][j] : 0;  // Paths from top
+                    int left = (j > 0) ? dp[i][j-1] : 0; // Paths from left
+                    if (i != 0 || j != 0) { // Skip the start position
+                        dp[i][j] = left + up;
+                    }
+
+
+                }
+                
+               
+            }
+        }
+        return dp[m-1][n-1];
 
     }
 };
