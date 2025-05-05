46a846ae2aa52186d50a744a457e80e0fd87a21e
Al-Amen <83497300+Al-amen@users.noreply.github.com>
2025-05-05 20:48:56 +0600

    Time: 838 ms (12.46%), Space: 26.2 MB (36.12%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,12 +1,13 @@
 class Solution {
 private:
-   bool f(string &s, int i, int j,vector<vector<int>>&dp) {
+   bool f(string &s, int i, int j,vector<vector<bool>>&dp) {
     
      if(i >= j)return true;
+     if(dp[i][j] != 0)return dp[i][j];
      if(s[i] == s[j]) {
-        return f(s,i+1,j-1,dp);
+        return dp[i][j] = f(s,i+1,j-1,dp);
      }
-     return false;
+     return dp[i][j] = 0;
 
    }
 public:
@@ -15,7 +16,7 @@ public:
         int n = s.size();
         int sp = -1;
         int mx = -1;
-        vector<vector<int>>dp(n+1,vector<int>(n+1,-1));
+        vector<vector<bool>>dp(n+1,vector<bool>(n+1,0));
         for(int i = 0; i < n; i++) {
             for(int j = i; j < n; j++) {
                 if(f(s,i,j,dp)) {
