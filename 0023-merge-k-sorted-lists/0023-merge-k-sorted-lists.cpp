ab1b5e2aa251c239714c56ec35dd768e90999182
Al-Amen <83497300+Al-amen@users.noreply.github.com>
2025-05-22 21:30:05 +0600

    Time: 2 ms (69.61%), Space: 18.5 MB (65.91%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    struct Compare {
+        bool operator()(ListNode* a, ListNode* b) {
+            return a->val > b->val; 
+        }
+    };
+
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, Compare> pq;
+
+        for (auto list : lists) {
+            if (list != nullptr) {
+                pq.push(list);
+            }
+        }
+
+        ListNode dummy(0);
+        ListNode* tail = &dummy;
+
+        while (!pq.empty()) {
+            ListNode* smallest = pq.top();
+            pq.pop();
+
+            tail->next = smallest;
+            tail = tail->next;
+
+            if (smallest->next) {
+                pq.push(smallest->next);
+            }
+        }
+
+        return dummy.next;
+    }
+};
