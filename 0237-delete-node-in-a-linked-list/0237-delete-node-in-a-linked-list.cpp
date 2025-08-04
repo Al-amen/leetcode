375a355be93108578e8fde3ef7a3bd776df5a332
Al-Amen <83497300+Al-amen@users.noreply.github.com>
2025-08-04 21:31:56 +0600

    Time: 3 ms (94.95%), Space: 12.5 MB (18.51%) - LeetHub

--- 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
+++ 0237-delete-node-in-a-linked-list/0237-delete-node-in-a-linked-list.cpp
@@ -10,10 +10,13 @@ class Solution {
 public:
     void deleteNode(ListNode* node) {
        // 1 → 2 → 3 → 4 → 5
-    node->val = node->next->val;         // Step 1: copy 4 into 3
-    ListNode* temp = node->next;         // Step 2: save pointer to node 4
-    node->next = node->next->next;       // Step 3: bypass node 4
-    delete temp;                         
+    // node->val = node->next->val;         // Step 1: copy 4 into 3
+    // ListNode* temp = node->next;         // Step 2: save pointer to node 4
+    // node->next = node->next->next;       // Step 3: bypass node 4
+    // delete temp;      
+
+    node->val = node->next->val;
+    node->next = node->next->next;                   
 
         
     }
