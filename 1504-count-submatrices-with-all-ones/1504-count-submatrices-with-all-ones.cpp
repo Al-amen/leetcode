7a1f5f933c6a8f4c4b03be16ca0f5b4561021c09
AL AMEN <83497300+Al-amen@users.noreply.github.com>
2025-08-21 22:06:13 +0600

    Time: 7 ms (76.48%), Space: 19.7 MB (35.37%) - LeetHub

---
+++ 1504-count-submatrices-with-all-ones/1504-count-submatrices-with-all-ones.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int numSubmat(vector<vector<int>>& mat) {
+        int m = mat.size(), n = mat[0].size();
+        int res = 0;
+        vector<vector<int>> row(m, vector<int>(n, 0));
+
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (j == 0) {
+                    row[i][j] = mat[i][j];
+                } else {
+                    row[i][j] = (mat[i][j] == 0) ? 0 : row[i][j - 1] + 1;
+                }
+                int cur = row[i][j];
+                for (int k = i; k >= 0; --k) {
+                    cur = min(cur, row[k][j]);
+                    if (cur == 0) {
+                        break;
+                    }
+                    res += cur;
+                }
+            }
+        }
+        return res;
+    }
+};
