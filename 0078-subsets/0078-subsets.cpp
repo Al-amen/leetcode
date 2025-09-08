2d8ae9500a8c29764ccf610393cc52b18bf1d01d
AL AMEN <83497300+Al-amen@users.noreply.github.com>
2025-09-08 11:17:25 +0600

    Time: 0 ms (100%), Space: 9.8 MB (80.79%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -3,15 +3,27 @@ class Solution {
 public:
     void f(vector<int>&nums, vector<int> & sub, vector<vector<int>>& res,int ind) {
         int n = nums.size();
-        if(ind == n) {
-            res.push_back(sub);
-            return ;
-        }
-        sub.push_back(nums[ind]);
-        f(nums,sub,res,ind+1);
-        sub.pop_back();
-        f(nums,sub,res,ind+1);
+        // if(ind == n) {
+        //     res.push_back(sub);
+        //     return ;
+        // }
+        // sub.push_back(nums[ind]);
+        // f(nums,sub,res,ind+1);
+        // sub.pop_back();
+        // f(nums,sub,res,ind+1);
 
+        // backtraing 
+
+        // 1 2 3 explore right elements of every elemt
+
+        res.push_back(sub);
+        for(int i = ind; i < n; i++) {
+            sub.push_back(nums[i]);
+            f(nums,sub,res,i+1);
+            sub.pop_back();
+
+        }
+        return ;
 
     }
     vector<vector<int>> subsets(vector<int>& nums) {
