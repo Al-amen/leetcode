a42b431dd0bddeff2b139d52b7665841eaff6fb6
AL AMEN <83497300+Al-amen@users.noreply.github.com>
2025-09-08 12:55:37 +0600

    Time: 0 ms (100%), Space: 10.5 MB (66.44%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -2,17 +2,29 @@ class Solution {
 public:
     void f(vector<int>&nums, vector<int>&sub, vector<vector<int>> &res, int ind) {
         int n = nums.size();
-        if(ind == n) {
-            res.push_back(sub);
-            return ;
+        // if(ind == n) {
+        //     res.push_back(sub);
+        //     return ;
+        // }
+        // sub.push_back(nums[ind]);
+        // f(nums,sub,res,ind+1);
+        // sub.pop_back();
+        // while(ind < n-1 && nums[ind] == nums[ind+1]) {
+        //     ind++;
+        // }
+        // f(nums,sub,res,ind+1);
+
+        // using backtracking 
+
+        res.push_back(sub);
+        for(int i = ind; i < n; i++) {
+            if(i != ind && nums[i] == nums[i-1]) {
+                continue;
+            }
+            sub.push_back(nums[i]);
+            f(nums,sub,res,i+1);
+            sub.pop_back();
         }
-        sub.push_back(nums[ind]);
-        f(nums,sub,res,ind+1);
-        sub.pop_back();
-        while(ind < n-1 && nums[ind] == nums[ind+1]) {
-            ind++;
-        }
-        f(nums,sub,res,ind+1);
 
     }
 
