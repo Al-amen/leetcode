c9658d9def87afb377ca18c28215c6e4a095d297
AL AMEN <83497300+Al-amen@users.noreply.github.com>
2025-12-02 22:39:40 +0600

    Time: 27 ms (43.45%), Space: 27 MB (96.49%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -1,18 +1,37 @@
 class Solution:
     def findRepeatedDnaSequences(self, s: str) -> List[str]:
-        from collections import defaultdict
+        if len(s) < 10:
+            return []
 
-        freq = defaultdict(int)
-        ans = set()
+        # Map DNA chars to digits
+        mp = {'A':0, 'C':1, 'G':2, 'T':3}
 
-        for i in range(len(s)-9):
-            print(f"{s[i:i+10]} -> {freq[s[i:i+10]]}")
-            if freq[s[i:i+10]] > 0:
-                ans.add(s[i:i+10])
-            freq[s[i:i+10]] += 1
+        B = 4               # base
+        L = 10              # window length
+        power = B**(L-1)    # B^9
 
-        return list(ans)
-            
+        h = 0               # current rolling hash
+        seen = set()        # hashes seen once
+        repeated = set()    # hashes seen more than once
 
+        # Build first window hash
+        for i in range(L):
+            h = h * B + mp[s[i]]
+
+        seen.add(h)
+
+        # Slide window
+        for i in range(1, len(s)-L+1):
+            left_char = mp[s[i-1]]
+            right_char = mp[s[i+L-1]]
+
+            # Rolling hash update
+            h = (h - left_char * power) * B + right_char
+
+            if h in seen:
+                repeated.add(s[i:i+L])
+            seen.add(h)
+
+        return list(repeated)
 
         
